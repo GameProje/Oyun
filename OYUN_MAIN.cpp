94bf432263bd6cd04460503c9c9c112cf8703fa7
menege13 <ege_ebt@hotmail.com>
2015-03-28 21:52:51 +0200

    Update OYUN_MAIN.cpp

--- OYUN_MAIN.cpp
+++ OYUN_MAIN.cpp
@@ -8,11 +8,11 @@ using namespace std;
 
 int main( )
 {
-   	initwindow( 900 , 600 , "WinBGIm" );
+   	initwindow( 900 , 607 , "WinBGIm" );
 	setbkcolor(renk_yan);
 	cleardevice();
 	setfillstyle(1,renk_ic);
-	bar(199,0,699,600);
+	bar(200,0,690,608);
 	
    
     
