93e2b77f242ed4437ebbc0a2c1eaec3cd59f6357
promegal <mehmetgokhanmeral@gmail.com>
2015-03-29 06:40:14 +0300

    Update alg.cpp

--- alg.cpp
+++ alg.cpp
@@ -1,59 +1,32 @@
-void blog_ciz(int , int, int);
-void blog_sil(int, int, int);
-int rastgele(int);
-
-
-void blog_ciz(int al, int h, int col){
-   
-          int sag;
-  
-           sag=180+al+105;  
-              setfillstyle(1,col);
-      bar(180,1+(h*32),180+al,33+(h*32));
-              setfillstyle(1,col);
-       bar(sag,1+(h*32),705,33+(h*32)); }   
-     
-     
-     
-void blog_sil(int al, int h, int col){
-   
-        int sag;
- 
-       sag=180+al+105;  
-              setfillstyle(1,renk_ic);
-        bar(180,1+(h*32),180+al,33+(h*32));
-              setfillstyle(1,renk_ic);
-       bar(sag,1+(h*32),705,33+(h*32));  }
-       
-       
-       
-       
-int rastgele(int count){
+                       if(k==0) r=rastgele(k);
+                  if(l==0) r1=rastgele(l);
+                  if(m==0) r2=rastgele(m);
+                  if(n==0) r3=rastgele(n);
+                  
+                 
+                  
+            if(i>14){ blog_ciz(r3,n,renk+13);n++; }         
+         if(i>9){ blog_ciz(r2,m,renk);m++; }
+         if(i>4){ blog_ciz(r1,l,renk+9);l++;}
+                 blog_ciz(r,k,renk+3); k++;
+           
+           
+           if(k>=19) k=0;
+                  if(l>=19) l=0;
+                  if(m>=19) m=0;
+                  if(n>=19) n=0;
+                  
+                  
+                  
+                  
+ int rastgele(int count){
     int ras;
-    if (count%27==0||count%27==4||count%27==8){ras=rand()%13; 
+    if (count==0){
+                srand(time(NULL));                                             
+          ras=rand()%13; 
           if(ras==0){ras=ras+1;}
           ras=ras*35;
 
           return ras;   }
 
-    }
-    
-    
-    
-    
-    
-    //Gerekli kısım
-    if(i%27==0) r=rastgele(i);
-                  if(i%27==4) r1=rastgele(i);
-                   if(i%27==8) r2=rastgele(i);
-                  k=i%27;
-                  l=k-4;
-                  m=k-8;
-           blog_ciz(r2,m,renk+3); 
-          blog_ciz(r1,l,renk+7);
-          blog_ciz(r,k,renk);
-                 delay(500);
-               blog_sil(r1,l,renk+7);
-          blog_sil(r,k,renk);   
-          blog_sil(r2,m,renk+3);  
-          i++;
+    }  
